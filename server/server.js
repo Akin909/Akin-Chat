9336795d4e076ef7840cfefa81ecd7ff93c49dc3
Akin909 <akin.sowemimo@gmail.com>
2017-03-13 23:40:57 +0000

    Added var to use of express rather than const

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3003;
 const publicPath = path.join(__dirname, '../public');
+var app = express();
 
+console.log(publicPath);
 app.use(express.static(publicPath));
   
 app.get('/', function(req, res) {
@@ -12,7 +13,6 @@ app.get('/', function(req, res) {
 
 
 
-console.log(publicPath);
 
 app.listen(port, function() {
   console.log('App server listening on port %d', port);
