463b2872179e68ddce84c5127db377049bb896c9
Akin909 <akin.sowemimo@gmail.com>
2017-03-22 21:08:06 +0000

    Separate index script into it a file and update server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,42 @@
+//*********************************
+//Server
+//*********************************
 const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 3003;
 const publicPath = path.join(__dirname, '../public');
 const socketIO = require('socket.io');
-const http = require ('http');
+const http = require('http');
 
 var app = express();
-var server = http.createServer( app );
+var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
-io.on('connection',(socket) => {
-  console.log('New user connected');  
+
+
+io.on('connection', (socket) => {
+
+  console.log('New user connected');
+
+
   socket.on('disconnect', () => {
-   console.log('Client disconnected'); 
+    console.log('Client disconnected');
+  });
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hey, what is going on.',
+    createdAt: 123
+  });
+
+  socket.emit('newMessage', {
+    from:'simon',
+    text:'Still hungover buddy',
+    createdAt:`${Date.now()}`
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('create Message', newMessage);
   });
 });
 
@@ -24,4 +47,3 @@ app.get('/', function(req, res) {
 server.listen(port, function() {
   console.log('App server listening on port %d', port);
 });
-
