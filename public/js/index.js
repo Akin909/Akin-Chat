d1600c8edb28b1aafc17f22ab85324c0d218f415
Akin909 <akin.sowemimo@gmail.com>
2017-03-22 21:09:00 +0000

    Add separated out client code and css

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+//*********************************
+//Client
+//*********************************
+var socket = io();
+socket.on('connect', function() {
+  console.log('connected to the server');
+
+
+  socket.on('newMessage', function(message) {
+    console.log('New message', message);
+  });
+
+
+  socket.emit('createMessage', {
+    from:'Akin',
+    text:'We hate you please DIE',
+    createdAt:Date.now()
+  });
+
+  socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+  });
+});
